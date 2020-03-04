51a1fcdd449a2f11a1d15c82ba866736932a646a
Kip Kip <pinskteckip@gmail.com>
2020-03-04 17:42:10 +0300

    Добавлен вывод системного времени

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+
+
 
 namespace Timer
 {
@@ -20,9 +24,25 @@ namespace Timer
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+
+         DispatcherTimer   timer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 1) }; // 1 секунда
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime dateTime = new DateTime();
+            dateTime = DateTime.Now;
+            
+            Clock.Content = dateTime.ToString().Substring(dateTime.ToString().Length - 9);
         }
     }
 }
