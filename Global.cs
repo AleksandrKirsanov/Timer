9cbbde345e5993302480a52ee3727d971afa8a51
Kip Kip <pinskteckip@gmail.com>
2020-03-30 14:16:55 +0300

    введены кнопки

---
+++ Global.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timer
+{
+   abstract class Global // Абстрактный класс глобальных переменных
+    {
+
+         
+     static DateTime  dateSet = DateTime.Now; //  Часы при срабатывании по часам
+        static public int Hour = dateSet.Hour;//  Минуты при срабатывании по часам
+        static public int Minute = dateSet.Minute;// Минуты при срабатывании по таймеру
+        static public int HourInterval = 0;// Часы при срабатывании по таймеру
+
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Diagnostics;
 
 
 
@@ -24,12 +25,18 @@ namespace Timer
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        DispatcherTimer timer1 = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 0, 0, 500) };
+        Stopwatch stopwatch = new Stopwatch();
+
+
         public MainWindow()
         {
             InitializeComponent();
 
-         DispatcherTimer   timer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 1) }; // 1 секунда
+            Hour.Content = Global.Hour;
+            Minute.Content = Global.Minute;
+
+            DispatcherTimer timer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 1) }; // 1 секунда
             timer.Tick += Timer_Tick;
             timer.Start();
 
@@ -37,12 +44,81 @@ namespace Timer
 
         }
 
-        private void Timer_Tick(object sender, EventArgs e)
+
+        private void Timer_Tick(object sender, EventArgs e) // таймер выводит на форму текущее время
         {
             DateTime dateTime = new DateTime();
             dateTime = DateTime.Now;
-            
+
             Clock.Content = dateTime.ToString().Substring(dateTime.ToString().Length - 9);
+        }
+
+        private void HourPlus_Click(object sender, RoutedEventArgs e) // 
+        {
+            if (Global.Hour < 24)
+            {
+                Global.Hour = Global.Hour + 1;
+            }
+            else
+            {
+                Global.Hour = 0;
+            }
+            Hour.Content = Global.Hour;
+
+
+
+        }
+
+
+
+
+        private void HourMinus_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (Global.Hour > 0)
+            {
+                Global.Hour--;
+            }
+            else
+            {
+                Global.Hour = 24;
+            }
+            Hour.Content = Global.Hour;
+
+        }
+
+        private void MinutePlus_Click(object sender, RoutedEventArgs e)
+        {
+            if (Global.Minute < 60)
+            {
+                Global.Minute++;
+            }
+            else
+            {
+                Global.Minute = 0;
+            }
+            Minute.Content = Global.Minute;
+        }
+
+        private void MinuteMinus_Click(object sender, RoutedEventArgs e)
+        {
+            if (Global.Minute > 0)
+            {
+                Global.Minute--;
+            }
+            else
+            {
+                Global.Minute = 60;
+            }
+            Minute.Content = Global.Minute;
+
+        }
+
+
+
+        private void HourPlus_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+           TimeInterval.Content = "ffffffffffffffff";
         }
     }
 }
